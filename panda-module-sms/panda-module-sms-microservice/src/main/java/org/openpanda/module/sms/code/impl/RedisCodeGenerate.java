eabfb38d3bae73e63e8f4acc8451402520f9d369
lingen.liu <lingen.liu@gmail.com>
2017-06-28 21:09:35 +0800

    Implement code generate class

--- panda-module-sms/panda-module-sms-microservice/src/main/java/org/openpanda/module/sms/code/impl/RedisCodeGenerate.java
+++ panda-module-sms/panda-module-sms-microservice/src/main/java/org/openpanda/module/sms/code/impl/RedisCodeGenerate.java
@@ -1,6 +1,9 @@
 package org.openpanda.module.sms.code.impl;
 
+import org.dayatang.cache.Cache;
+import org.dayatang.cache.redis.RedisBasedCache;
 import org.openpanda.module.sms.code.CodeGenerate;
+import org.openpanda.module.sms.util.RandomCodeUtil;
 
 /**
  * Created by lingen on 2017/6/21.
@@ -8,13 +11,37 @@ import org.openpanda.module.sms.code.CodeGenerate;
  */
 public class RedisCodeGenerate implements CodeGenerate {
 
+    private static String PREFIX = "CODE_GENERATE_";
+
+    private String ip;
+
+    private int port;
+
+    private String password;
+
+    private Cache cache;
+
+    public RedisCodeGenerate(String ip,int port,String password){
+        this.ip = ip;
+        this.port = port;
+        this.password = password;
+        this.cache = new RedisBasedCache(ip,port,password);
+    }
+
     @Override
     public String generateCode(String mobile) {
-        return null;
+        String validCode = RandomCodeUtil.randomSix();
+        cache.put(PREFIX+mobile,validCode,  60 * 2);
+        return validCode;
     }
 
     @Override
     public boolean validCode(String mobile, String code) {
+
+        String validCode = (String) cache.get(PREFIX+mobile);
+        if (validCode.equals(code)){
+            return true;
+        }
         return false;
     }
 }
